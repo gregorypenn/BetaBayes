82c771e8af01e0680b09e739e3757a753bb1dbab
Gregory Penn <gregory@crediblecorvid.com>
2023-02-17 19:38:22 -0700

    Removed examples pending GitHub Pages fix.

--- R/betaCI.R
+++ R/betaCI.R
@@ -3,9 +3,6 @@
 #' @param shape2 Shape parameter passed to beta inverse cumulative distribution function qbeta.
 #' @param CImass Central probability mass.
 #' @return A numeric vector of the two quantiles bounding the central probability mass.
-#' @examples
-#' betaCI(6, 4)
-#' betaCI(2, 50, 0.75)
 #' @export
 betaCI <- function(shape1, shape2, CImass = 0.95) {
   return(HDIofICDF(stats::qbeta, shape1 = shape1, shape2 = shape2))
